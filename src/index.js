0c834f017317abf2d8274568c6a9847705e97cb7
Shulhan Andrei <shulhan.andrei@gmail.com>
2020-03-22 17:34:37 +0300

    feat: finished task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,11 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+    for (let i = 0; i < bracketsConfig.length; i++) {
+        while (true) {
+            if (str.includes(bracketsConfig[i].join(''))) {
+                str = str.replace(bracketsConfig[i].join(''), '');
+                i = 0;
+            } else break;
+        }
+    }
+    return str ? false : true;
 }
